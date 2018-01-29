2b7ca7cced912f89af6dba0615d06e2a7b21226d
Casey Lund <caseylund21@gmail.com>
2018-01-29 14:37:59 -0700

    started with variables

---
+++ 006_variables.js
@@ -0,0 +1,14 @@
+// var fullName : string = "Casey Lund";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+// fullName = "Sydney Lund";
+// paidAccount = false;
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+function printName(f, l) {
+    var greeting = "Hi there, ";
+    console.log(greeting + f + " " + l);
+}
+printName("Casey", "Lund");
+//# sourceMappingURL=006_variables.js.map
---
+++ 006_variables.ts
@@ -0,0 +1,21 @@
+// var fullName : string = "Casey Lund";
+// let paidAccount : boolean = true;
+// const versionNumber : number = 1.3;
+
+// fullName = "Sydney Lund";
+// paidAccount = false;
+
+// console.log(fullName);
+// console.log(paidAccount);
+// console.log(versionNumber);
+
+
+function printName(f, l) {
+  var greeting : string = "Hi there, ";
+  console.log(greeting + f + " " + l);
+
+  var greeting : string = "Hi there, ";
+  console.log(greeting + f + " " + l);
+}
+
+printName("Casey", "Lund");
